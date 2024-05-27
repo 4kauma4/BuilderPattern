4886771f288a4104b0456c2d2980200c675ace7c
Dobby <159747473+4kauma4@users.noreply.github.com>
2024-05-27 14:02:06 +0700

    Add files via upload

---
+++ BuilderPattren/Dog.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattren
+{
+    public class Dog
+    {
+        private string _name;
+        private string _breed;
+        private int _age;
+        private List<string> _toys;
+
+        // приват конструктор
+        private Dog() { }
+
+        // аксессор
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetBreed()
+        {
+            return _breed;
+        }
+
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        public List<string> GetToys()
+        {
+            return new List<string>(_toys); 
+        }
+
+        // строитель
+        public class DogBuilder
+        {
+            private string _name;
+            private string _breed;
+            private int _age;
+            private List<string> _toys;
+
+            public DogBuilder()
+            {
+                _toys = new List<string>();
+            }
+
+            // мутатор
+            public DogBuilder SetName(string name)
+            {
+                _name = name;
+                return this; // ввозврыщаем для цепочки выозовов
+            }
+
+            
+            public DogBuilder SetBreed(string breed)
+            {
+                _breed = breed;
+                return this; 
+            }
+
+           
+            public DogBuilder SetAge(int age)
+            {
+                _age = age;
+                return this; 
+            }
+
+            
+            public DogBuilder AddToy(string toy)
+            {
+                _toys.Add(toy);
+                return this; 
+            }
+
+            // объект
+            public Dog Build()
+            {
+                Dog dog = new Dog();
+                dog._name = _name;
+                dog._breed = _breed;
+                dog._age = _age;
+                dog._toys = new List<string>(_toys); 
+                return dog;
+            }
+        }
+    }
+}
---
+++ BuilderPattren/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BuilderPattren
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog.DogBuilder()
+                .SetName("Вулкан")
+                .SetBreed("Дворняжка")
+                .SetAge(5)
+                .AddToy("Косточка")
+                .AddToy("Мячик")
+                .Build();
+
+            Console.WriteLine("Коичка: " + dog.GetName());
+            Console.WriteLine("Попода: " + dog.GetBreed());
+            Console.WriteLine("Возраст: " + dog.GetAge());
+            Console.WriteLine("Игрушки: " + string.Join(", ", dog.GetToys()));
+        }
+    }
+}
